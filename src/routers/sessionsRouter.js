ad5ecfa840924c6596168d123862074e2f90e40e
Kristina Bekher <krbekher@gmail.com>
2024-04-09 08:59:11 +0200

    add files

---
+++ src/routers/sessionsRouter.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const debug = require("debug")("app:sessionRouter");
+const { MongoClient, ObjectID } = require("mongodb");
+
+// const sessions = require("../data/sessions.json");
+
+const sessionsRouter = express.Router();
+
+sessionsRouter.route("/").get((req, res) => {
+  const url =
+    "mongodb+srv://bekherkr:dFqjsSKxcPqAUrqO@globomantics.wle81qe.mongodb.net/?retryWrites=true&w=majority&appName=Globomantics";
+  const dbName = "globomantics";
+
+  (async function mongo() {
+    let client;
+    try {
+      client = await MongoClient.connect(url);
+      debug("Connetcted to the mongo DB");
+
+      const db = client.db(dbName);
+
+      const sessions = await db.collection('sessions').find().toArray();
+
+      res.render('sessions', { sessions });
+    } catch (error) {
+      debug(error.stack);
+    }
+    client.close();
+  })();
+});
+
+sessionsRouter.route("/:id").get((req, res) => {
+  const id = req.params.id;
+  const url =
+  "mongodb+srv://bekherkr:dFqjsSKxcPqAUrqO@globomantics.wle81qe.mongodb.net/?retryWrites=true&w=majority&appName=Globomantics";
+const dbName = "globomantics";
+
+(async function mongo() {
+  let client;
+  try {
+    client = await MongoClient.connect(url);
+    debug("Connetcted to the mongo DB");
+
+    const db = client.db(dbName);
+
+    const session = await db
+      .collection('sessions')
+      .findOne({_id: new ObjectID(id)});
+
+    res.render("session", { session });
+
+  } catch (error) {
+    debug(error.stack);
+  }
+  client.close();
+})();
+
+  
+});
+
+module.exports = sessionsRouter;
